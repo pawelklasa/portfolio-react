4a042010398a036381ab517c8684f724d301151f
Pawel Klasa <pawel.klasa@MAC000170.local>
2017-11-29 22:46:36 +0000

    li styling and hover link

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -10,24 +10,24 @@ export class NavBar extends React.Component {
     render() {
         return (
             
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg">
                 <Link to='/'><span class="navbar-brand">pav_ka</span></Link>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarText">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active">
+                <div className="collapse navbar-collapse" id="navbarText">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active mr-2">
                             <Link to='/'>Home</Link>
                         </li>
-                        <li class="nav-item">
+                        <li className="nav-item mr-2">
                             <Link to='/features'>Features</Link>
                         </li>
-                        <li class="nav-item">
+                        <li className="nav-item mr-2">
                             <Link to='/contact'>Contact</Link>
                         </li>
                     </ul>
-                    <span class="navbar-text">
+                    <span className="navbar-text">
                         Navbar text with an inline element
                     </span>
                 </div>
