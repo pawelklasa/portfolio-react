04e46f93f9db37c3a03770f55c689f7f370926b8
Pawel Klasa <pawel.klasa@MAC000170.local>
2017-11-29 19:00:51 +0000

    cleaning up the code

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -6,12 +6,14 @@ import {
 export class Contact extends React.Component { 
     render() {
         return (
+            
             <div className="container">
                 <div classNAme="bg-light">
                     <h1>Contact</h1>
                     <p>some content goes here</p>
                 </div>
             </div>
+
         )
     }
 }
