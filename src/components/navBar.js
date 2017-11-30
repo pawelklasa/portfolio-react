9593fc6b205ce6e1c37f024bb9fad97f801dd898
Pawel Klasa <pawel.klasa@gmail.com>
2017-11-30 09:35:02 +0000

    font awesome install and burger menu

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -12,7 +12,7 @@ export class NavBar extends React.Component {
             <nav className="navbar navbar-expand-lg">
                 <Link to='/'><span class="navbar-brand">pav_ka</span></Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon">=</span>
+                    <span className="navbar-toggler-icon"><i class="fa fa-bars" aria-hidden="true"></i></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarText">
                     <ul className="navbar-nav mr-auto">
