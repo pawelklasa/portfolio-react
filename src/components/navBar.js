aefe0dab3dc05776eb39ba543e24f826449405b4
Pawel Klasa <pawel.klasa@gmail.com>
2017-12-04 15:31:39 +0000

    fixed menu

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -11,7 +11,7 @@ export class NavBar extends React.Component {
         return (
             <nav className="navbar navbar-expand-lg">
                 <Link to='/'><span class="navbar-brand"><i class="fa fa-terminal" aria-hidden="true"></i> pav_ka</span></Link>
-                <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"><i class="fa fa-bars" aria-hidden="true"></i></span>
                 </button>
                 <div className="collapse navbar-collapse flex-row-reverse" id="navbarText">
