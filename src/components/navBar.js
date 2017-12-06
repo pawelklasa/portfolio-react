e080326bcaaf94acdaf1cd26c7a5676ca2dc0871
Pawel Klasa <pawel.klasa@gmail.com>
2017-12-06 14:54:46 +0000

    centering nav

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -4,13 +4,12 @@ import {
 } from 'react-router-dom';
 
 //css
-
 import '../assets/css/navBar.css'
 
 export class NavBar extends React.Component {
     render() {
         return (
-            <nav className="navbar navbar-expand-lg fixed-top">
+            <nav className="navbar navbar-expand-lg">
                 <Link to='/'><span class="navbar-brand">&gt;<span className="blinking-cursor">_ </span>pav_ka</span></Link>
                 <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"><i class="fa fa-bars" aria-hidden="true"></i></span>
